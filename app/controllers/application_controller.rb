dfe9eaf9e0b6e9ce0c7247a5de8a4c3b43546da9
Takahide Sato <sato_t@cyberowl.co.jp>
2024-05-26 20:29:37 +0900

    set db connection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  def index
+    render json: {
+      message: 'hello world'
+    }
+  end
+
+  def health_check
+    render json: {
+      ok: true,
+    }
+  end
 end
